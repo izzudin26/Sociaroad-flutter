53c36cf72042199b026843656388e8f3e7a55007
izzudin26 <izzudin.ar26@gmail.com>
2021-11-15 08:21:22 +0700

    disable banner debug

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Society Road',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         // This is the theme of your application.
         //
