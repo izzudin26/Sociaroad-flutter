40264e833af6c670634eb078b9e9924a3e7bbdfa
izzudin26 <izzudin.ar26@gmail.com>
2021-11-16 14:03:02 +0700

    fixing error firebase init

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:society_road/login.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:society_road/navigation.dart';
+import 'package:society_road/registration.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
@@ -15,18 +19,15 @@ class MyApp extends StatelessWidget {
       title: 'Society Road',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: Login(),
+      initialRoute: '/login',
+      routes: {
+        '/login': (context) => Login(),
+        '/register': (context) => Registration(),
+        '/navigation': (context) => NavigationApp()
+      },
     );
   }
 }
