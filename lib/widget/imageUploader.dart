4dbe18eb33ab4f748c7fdd59b1fae85a2025f1c9
izzudin26 <izzudin.ar26@gmail.com>
2021-12-02 03:36:43 +0700

    integration with api create report

--- lib/widget/imageUploader.dart
+++ lib/widget/imageUploader.dart
@@ -3,9 +3,13 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:society_road/model/imagePickerModel.dart';
+import 'package:society_road/webservice/reportService.dart';
+import 'package:society_road/webservice/url.dart';
+import 'package:society_road/widget/snackbarAlert.dart';
 
 class ImageUploader extends StatefulWidget {
-  ImageUploader({Key? key}) : super(key: key);
+  int reportId;
+  ImageUploader({Key? key, required this.reportId}) : super(key: key);
 
   @override
   _ImageUploaderState createState() => _ImageUploaderState();
@@ -14,17 +18,24 @@ class ImageUploader extends StatefulWidget {
 class _ImageUploaderState extends State<ImageUploader> {
   late XFile image;
 
-  List<String> images = [];
+  List<ImagePickerModel> images = [];
   final ImagePicker _picker = ImagePicker();
+
+  bool isProcessing = false;
 
   void pickImage(String type) async {
     try {
       XFile? img = await _picker.pickImage(
           source: ImageSource.gallery, maxHeight: 300, maxWidth: 300);
       if (img != null) {
-        print(img.path);
         setState(() {
-          images.add(img.path);
+          isProcessing = true;
+        });
+        ImagePickerModel image = await ReportService.uploadImage(
+            reportId: widget.reportId, image: new File(img.path));
+        setState(() {
+          images.add(image);
+          isProcessing = false;
         });
         print(images);
       }
@@ -33,17 +44,46 @@ class _ImageUploaderState extends State<ImageUploader> {
     }
   }
 
+  void removeImage(int index) async {
+    print("hit");
+    try {
+      await ReportService.removeAssetImage(imageId: images[index].id!);
+      setState(() {
+        images.removeAt(index);
+      });
+    } catch (e) {
+      showSnackbar(context, e.toString());
+    }
+  }
+
+  void doUpload() async {
+    Navigator.pushReplacementNamed(context, "/navigation");
+  }
+
   Widget showImages() {
     return Container(
       child: Column(
           children: images
-              .map((e) => Stack(
-                    children: [
-                      Container(
-                        child: Image.file(File(e)),
-                      )
-                    ],
-                  ))
+              .asMap()
+              .map((i, e) => MapEntry(
+                  i,
+                  Container(
+                    margin: EdgeInsets.only(top: 10, bottom: 10),
+                    child: Stack(
+                      children: [
+                        Container(
+                          child: Image.network(
+                              '$serverUrl/asset/${e.locationPath}'),
+                        ),
+                        IconButton(
+                            onPressed: () {
+                              removeImage(i);
+                            },
+                            icon: Icon(Icons.close, color: Colors.red))
+                      ],
+                    ),
+                  )))
+              .values
               .toList()),
     );
   }
@@ -52,8 +92,21 @@ class _ImageUploaderState extends State<ImageUploader> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.white,
         automaticallyImplyLeading: false,
-        title: Text("Upload Gambar"),
+        title: Text("Upload Gambar", style: TextStyle(color: Colors.blue)),
+        bottom: !isProcessing
+            ? null
+            : PreferredSize(
+                child: LinearProgressIndicator(),
+                preferredSize: Size.fromHeight(.5)),
+        actions: [
+          TextButton(
+              onPressed: () {
+                doUpload();
+              },
+              child: Text("Laporkan", style: TextStyle(color: Colors.blue)))
+        ],
       ),
       body: SingleChildScrollView(
         child: Container(
