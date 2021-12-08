b7d58b9e411b2abff9152b8e918e5b3ab5d8af34
izzudin26 <izzudin.ar26@gmail.com>
2021-12-08 08:51:44 +0700

    change view

--- lib/widget/report.dart
+++ lib/widget/report.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:society_road/model/reportModel.dart';
+import 'package:society_road/webservice/reportService.dart';
 
 class ReportWidget extends StatefulWidget {
   ReportWidget({Key? key}) : super(key: key);
@@ -8,12 +10,72 @@ class ReportWidget extends StatefulWidget {
 }
 
 class _ReportWidgetState extends State<ReportWidget> {
+  List<ReportModel> data = [];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    doFetchData();
+  }
+
+  void doFetchData() async {
+    try {
+      List<ReportModel> resultFetch = await ReportService.getPersonReport();
+      setState(() {
+        data = resultFetch;
+      });
+      print(resultFetch);
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Widget DataReport() {
+    Size size = MediaQuery.of(context).size;
+    return Column(
+      children: data
+          .map((e) => (Container(
+                margin: EdgeInsets.all(10),
+                padding: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(10),
+                      topRight: Radius.circular(10),
+                      bottomLeft: Radius.circular(10),
+                      bottomRight: Radius.circular(10)),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 1,
+                      blurRadius: 5,
+                      offset: Offset(0, 3), // changes position of shadow
+                    ),
+                  ],
+                ),
+                child: ListTile(
+                  title: Text(e.address),
+                  subtitle: Text('${e.city} - ${e.province}'),
+                  trailing: Icon(Icons.location_on),
+                ),
+              )))
+          .toList(),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text("Report widget"),
-      ),
-    );
+    return Scaffold(
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          title: Text("Laporan Anda"),
+        ),
+        body: Container(
+          padding: EdgeInsets.all(20),
+          child: ListView(
+            children: [data.length > 0 ? DataReport() : SizedBox()],
+          ),
+        ));
   }
 }
