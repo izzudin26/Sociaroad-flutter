18cbb57c3b97b7ec890c703eba48cbb8616992d9
izzudin26 <izzudin.ar26@gmail.com>
2021-12-08 13:04:58 +0700

    add map launcher

--- lib/widget/feed.dart
+++ lib/widget/feed.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:society_road/model/reportModel.dart';
 import 'package:society_road/webservice/reportService.dart';
 import 'package:society_road/webservice/url.dart';
+import 'package:society_road/widget/snackbarAlert.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:map_launcher/map_launcher.dart';
 
 class FeedWidget extends StatefulWidget {
   FeedWidget({Key? key}) : super(key: key);
@@ -21,8 +25,45 @@ class _FeedWidgetState extends State<FeedWidget> {
     doFetchData();
   }
 
+  Future<String> getCity() async {
+    Position currentPosition = await _determinePosition(context);
+    List<Placemark> placemark = await placemarkFromCoordinates(
+        currentPosition.latitude, currentPosition.latitude);
+    print(placemark);
+    return placemark[0].subAdministrativeArea!;
+  }
+
+  Future<Position> _determinePosition(BuildContext context) async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      showSnackbar(context, 'Mohon Aktifkan GPS !');
+      await Geolocator.openLocationSettings();
+    }
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        showSnackbar(context, 'Mohon Aktifkan Perizinan GPS pada aplikasi');
+        await Geolocator.openAppSettings();
+      }
+    }
+    if (permission == LocationPermission.deniedForever) {
+      showSnackbar(context,
+          'Location permissions are permanently denied, we cannot request permissions.');
+      await Geolocator.openLocationSettings();
+    }
+
+    return await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+  }
+
   void doFetchData() async {
     try {
+      String city = await getCity();
+      print(city);
       List<ReportModel> resultFetch = await ReportService.getCollectionReport(
           city: "Kabupaten Lamongan", page: 1);
       setState(() {
@@ -33,6 +74,15 @@ class _FeedWidgetState extends State<FeedWidget> {
     } catch (e) {
       print(e);
     }
+  }
+
+  void gotoMap(double lang, double lat) async {
+    final availableMaps = await MapLauncher.installedMaps;
+    print(
+        availableMaps); // [AvailableMap { mapName: Google Maps, mapType: google }, ...]
+
+    await availableMaps.first
+        .showMarker(coords: Coords(lat, lang), title: "OKE JEK");
   }
 
   Widget DataReport() {
@@ -62,6 +112,9 @@ class _FeedWidgetState extends State<FeedWidget> {
                   title: Text(e.address),
                   subtitle: Text('${e.city} - ${e.province}'),
                   trailing: Icon(Icons.location_on),
+                  onTap: () {
+                    gotoMap(double.parse(e.lang), double.parse(e.lat));
+                  },
                 ),
               )))
           .toList(),
@@ -73,7 +126,7 @@ class _FeedWidgetState extends State<FeedWidget> {
     return Scaffold(
         appBar: AppBar(
           automaticallyImplyLeading: false,
-          title: Text("Jalan Rusak Terdekat"),
+          title: Text("Jalan Rusak Sekitar"),
           bottom: !isProcessing
               ? null
               : PreferredSize(
